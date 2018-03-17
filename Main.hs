70cecf7716f6dfc880a361a9862ed1ab3b412ce0
Alexey Lebedeff <binarin@binarin.ru>
2018-03-17 15:21:43 +0100

    Implement an example from threepenny docs

--- Main.hs
+++ Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import qualified Graphics.UI.Threepenny       as UI
+import           Graphics.UI.Threepenny.Core
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    startGUI defaultConfig
+        { jsPort       = Just 8023
+        , jsStatic     = Just "../wwwroot"
+        } setup
+
+setup :: Window -> UI ()
+setup window = do
+  return window # set UI.title "Hello World!"
+  button <- UI.button # set UI.text "Click me!"
+  getBody window #+ [element button]
+  on UI.click button $ const $ do
+    element button # set UI.text "I have been clicked!"
